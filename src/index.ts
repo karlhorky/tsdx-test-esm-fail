a1eb5c1ce3ad08873fff76bbd5550112879493e3
Karl Horky <karl.horky@gmail.com>
2021-04-23 11:34:00 +0200

    Add minimal reproduction

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import pMap from 'p-map';
+
+console.log('p-map', pMap);
+
+export const sum = (a: number, b: number) => {
+  if ('development' === process.env.NODE_ENV) {
+    console.log('boop');
+  }
+  return a + b;
+};
